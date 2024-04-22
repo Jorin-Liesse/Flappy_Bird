43eeb37c200613ab6014f32a018f6dcb9d9475b8
Jorin Liesse <liesse.jorin@gmail.com>
2024-04-22 23:04:50 +0200

    update

--- script.js
+++ script.js
@@ -34,8 +34,5 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('service-worker.js')
-      .then(() => {
-        console.log("Service Worker Registered");
-      });
   });
 }
